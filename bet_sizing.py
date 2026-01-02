f1e8616236f6e0ce86c8ce92e3939fc284e79ed2
Jayblack23 <jason.chappell87@yahoo.com>
2026-01-02 18:17:06 -0500

    Remove docstring from bet_sizing.py

--- bet_sizing.py
+++ bet_sizing.py
@@ -1,6 +1,3 @@
-"""
-Bet sizing calculations for NCAA basketball predictions.
-"""
 def kelly(prob, odds=-110, bankroll=1000, fraction=0.5):
     b = abs(odds) / 100
     k = (prob * (b + 1) - 1) / b
